d52136cae6cfefe31a15bca249195f53753af71a
cryptickoan <me@cryptickoan.com>
2021-09-20 21:45:22 -0600

    Bug fix
    Chanched how current apr was being parsed... there goes two hours :(

--- src/hooks/pool2/usePool2APR.ts
+++ src/hooks/pool2/usePool2APR.ts
@@ -1,6 +1,8 @@
 import { useQuery } from "react-query";
 import { useRari } from "context/RariContext";
 
+import { utils } from 'ethers';
+
 export const usePool2APR = () => {
   const { rari } = useRari();
 
@@ -9,12 +11,12 @@ export const usePool2APR = () => {
     const tvl = await rari.governance.rgt.sushiSwapDistributions.totalStakedUsd();
     return (
       parseInt(
-        (
+        utils.formatEther(
           await rari.governance.rgt.sushiSwapDistributions.getCurrentApr(
             blockNumber,
             tvl
           )
-        ).toString()
+        )
       ) / 1e16
     ).toFixed(2);
   });
