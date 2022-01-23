72035b93e462dda87854c6691fb73e0804941ea2
cryptickoan <me@cryptickoan.com>
2022-01-22 19:21:57 -0600

    Fixed

--- src/hooks/fuse/useCheckUniV2Oracle.ts
+++ src/hooks/fuse/useCheckUniV2Oracle.ts
@@ -4,10 +4,23 @@ import { ChainID } from "esm/utils/networks"
 import { useQuery } from "react-query"
 import UniswapV2OracleAbi  from '../../esm/Fuse/contracts/abi/UniswapTwapPriceOracleV2Root.json'
 
-const useCheckUniV2Oracle = (underlying: string, baseToken: string) => {
+const useCheckUniV2Oracle = (
+    underlying: string, 
+    baseToken: string, 
+    activeOracleModel: string
+) => {
+
     const { fuse, chainId } = useRari()
 
     const { data: isItReady, error } = useQuery(`Checking Twap Oracle for ${underlying} ${baseToken} ${fuse.addresses.UNISWAP_TWAP_PRICE_ORACLE_V2_FACTORY_CONTRACT_ADDRESS} `, async () => {
+        if (
+            activeOracleModel === "Default_Price_Oracle" 
+            || activeOracleModel === "Current_Price_Oracle"
+            || activeOracleModel === "Rari_MasterPriceOracle"
+            || activeOracleModel === "Chainlink_Oracle"
+            || activeOracleModel === "Uniswap_V3_Oracle"
+            || activeOracleModel === "Custom_Oracle" 
+        ) return null 
         
         // Its interpreted as sushiswap on arbitrum. 
         const OracleContract = new Contract(
@@ -19,7 +32,7 @@ const useCheckUniV2Oracle = (underlying: string, baseToken: string) => {
         const isItReady = await OracleContract.callStatic.price(
             underlying, // will be underlying
             baseToken, // will be base token
-            fuse.addresses.UNISWAP_TWAP_PRICE_ORACLE_V2_ROOT_CONTRACT_ADDRESS
+            fuse.addresses.UNISWAP_V2_FACTORY_ADDRESS
         )
 
         console.log({isItReady})
