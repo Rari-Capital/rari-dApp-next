1838ef018c8b091d41b74c7785c7a6aea189abc9
cryptickoan <me@cryptickoan.com>
2022-01-24 02:23:53 -0600

    :bug: Fixed
    Hook will not refetch unless user is deploying a uniswap v2 oracle

--- src/hooks/fuse/useCheckUniV2Oracle.ts
+++ src/hooks/fuse/useCheckUniV2Oracle.ts
@@ -11,16 +11,11 @@ const useCheckUniV2Oracle = (
 ) => {
 
     const { fuse, chainId } = useRari()
+    const shouldCheck = activeOracleModel === "Uniswap_V2_Oracle" 
+    || activeOracleModel === "SushiSwap_Oracle" ? true : false
 
     const { data: isItReady, error } = useQuery(`Checking Twap Oracle for ${underlying} ${baseToken} ${fuse.addresses.UNISWAP_TWAP_PRICE_ORACLE_V2_FACTORY_CONTRACT_ADDRESS} `, async () => {
-        if (
-            activeOracleModel === "Default_Price_Oracle" 
-            || activeOracleModel === "Current_Price_Oracle"
-            || activeOracleModel === "Rari_MasterPriceOracle"
-            || activeOracleModel === "Chainlink_Oracle"
-            || activeOracleModel === "Uniswap_V3_Oracle"
-            || activeOracleModel === "Custom_Oracle" 
-        ) return null 
+        if (!shouldCheck) return null 
         
         // Its interpreted as sushiswap on arbitrum. 
         const OracleContract = new Contract(
@@ -28,7 +23,6 @@ const useCheckUniV2Oracle = (
             UniswapV2OracleAbi,
             fuse.provider
         )
-            console.log('before')
         const isItReady = await OracleContract.callStatic.price(
             underlying, // will be underlying
             baseToken, // will be base token
@@ -40,10 +34,8 @@ const useCheckUniV2Oracle = (
         return !!isItReady
     }, {
         // Refetch the data every second
-        refetchInterval: 1000,
+        refetchInterval: shouldCheck ? 1000 : false,
       })
-
-    console.log({isItReady})
 
     return isItReady
 
