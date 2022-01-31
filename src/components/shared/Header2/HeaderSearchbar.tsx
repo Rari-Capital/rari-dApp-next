fc868bfe98ed0d3bcb075b1232cba647e107ffc5
nathanhleung <github@nathanhleung.com>
2022-01-31 09:47:33 -0800

    Fix mobile header bug

--- src/components/shared/Header2/HeaderSearchbar.tsx
+++ src/components/shared/Header2/HeaderSearchbar.tsx
@@ -1,13 +1,8 @@
-import { Box } from "@chakra-ui/react";
 import React from "react";
 import Searchbar from "../Searchbar";
 
-const HeaderSearchbar: React.FC<React.ComponentProps<typeof Box>> = (props) => {
-  return (
-    <Box w={800} mx={5} my="auto " alignSelf="flex-start" {...props}>
-      <Searchbar height="33px" smaller />
-    </Box>
-  );
+const HeaderSearchbar: React.FC = () => {
+  return <Searchbar width="375px" height="33px" smaller />;
 };
 
 export default HeaderSearchbar;
