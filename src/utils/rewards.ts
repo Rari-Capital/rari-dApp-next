08ed98e38e9d58a5e06e82108cf7b16cd5edb311
sharad-s <sharadshekar@gmail.com>
2022-02-14 11:56:10 -0600

    fixed RewardsDistro

--- src/utils/rewards.ts
+++ src/utils/rewards.ts
@@ -1,26 +1,29 @@
+import { providers } from "@0xsequence/multicall";
 import { Fuse } from "esm";
+import { utils } from "ethers";
 import { createRewardsDistributor } from "./createComptroller";
-// import { sendWithMultiCall } from "./multicall";
+import { sendWithMultiCall } from "./multicall";
 
 export const claimRewardsFromRewardsDistributors = async (
   fuse: Fuse,
   address: string,
   rewardsDistributors: string[]
 ) => {
-  const methods = rewardsDistributors.map((rDAddress: string) => {
-    const rd = createRewardsDistributor(rDAddress, fuse);
-    console.log({ rd }, rd.claimRewards);
-    return rd.claimRewards(address);
+  const encodedCalls = rewardsDistributors.map((rDAddress: string) => {
+    // const rd = createRewardsDistributor(rDAddress, fuse);
+    let iface = new utils.Interface(
+      fuse.compoundContracts[
+        "contracts/RewardsDistributorDelegate.sol:RewardsDistributorDelegate"
+      ].abi
+    );
+    const callData = iface.encodeFunctionData("claimRewards", [address]);
+    return [rDAddress, callData];
+
+    // return rd.interface.function claimRewards(address);
   });
 
-  const addrs = rewardsDistributors;
-  alert("YO2");
 
-  const encodedCalls = methods.map((m, i) => {
-    return [addrs[i], m.encodeABI()];
-  });
+  const returnDatas = await sendWithMultiCall(fuse, encodedCalls, address);
 
-  console.log({ encodedCalls });
-
-  // const returnDatas = await sendWithMultiCall(fuse.web3, encodedCalls, address);
+  console.log({ returnDatas });
 };
