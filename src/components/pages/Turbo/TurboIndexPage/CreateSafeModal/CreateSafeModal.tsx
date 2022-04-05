38500cdaf071d28d097e25c852b9a5a644e22739
cryptickoan <me@cryptickoan.com>
2022-04-05 16:56:01 -0600

    Comments and stats edition

--- src/components/pages/Turbo/TurboIndexPage/CreateSafeModal/CreateSafeModal.tsx
+++ src/components/pages/Turbo/TurboIndexPage/CreateSafeModal/CreateSafeModal.tsx
@@ -1,6 +1,6 @@
 import { create } from "domain";
 import { useRari } from "context/RariContext";
-import { parseEther } from "ethers/lib/utils";
+import { formatEther, parseEther } from "ethers/lib/utils";
 import useHasApproval from "hooks/useHasApproval";
 import { createSafeAndDeposit } from "lib/turbo/transactions/createSafeAndDeposit";
 import { TRIBE, TurboAddresses } from "lib/turbo/utils/constants";
@@ -12,6 +12,7 @@ import { useToast } from "@chakra-ui/react";
 import { CreateSafeCtx, MODAL_STEPS } from "./modalSteps";
 import { createSafe } from "lib/turbo/transactions/safe";
 import { approve, checkAllowance } from "utils/erc20Utils";
+import { useBalanceOf } from "hooks/useBalanceOf";
 
 type CreateSafeModalProps = Pick<
   React.ComponentProps<typeof Modal>,
@@ -22,46 +23,45 @@ export const CreateSafeModal: React.FC<CreateSafeModalProps> = ({
   isOpen,
   onClose,
 }) => {
+  // Rari and NextJs
   const router = useRouter();
   const { address, provider, chainId } = useRari();
-  const [stepIndex, setStepIndex] = useState(0);
+  const toast = useToast();
 
+  // Modal State
+  const [stepIndex, setStepIndex] = useState(0);
   function incrementStepIndex() {
     if (stepIndex + 1 !== MODAL_STEPS.length) {
       setStepIndex(stepIndex + 1);
     }
   }
+  // Modal Buttons
+  const [approving, setApproving] = useState(false);
+  const [creatingSafe, setCreatingSafe] = useState(false);
+  const [navigating, setNavigating] = useState(false);
 
-  // TODO(sharad-s): Don't hardcode this
-  const underlyingTokenAddresses = [
-    // TRIBE
-    "0xc7283b66Eb1EB5FB86327f08e1B5816b0720212B",
-    // DAI
-    "0x6b175474e89094c44da98b954eedeac495271d0f",
-  ];
-
+  // Safe's chosen underlying asset
+  const underlyingTokenAddresses = [TRIBE];
   const [underlyingTokenAddress, setUnderlyingTokenAddress] = useState(TRIBE);
+  const collateralBalance = useBalanceOf(address, underlyingTokenAddress);
+
+  // Used only if user will deposit after creating.
   const [depositAmount, setDepositAmount] = useState<string>();
-  const [boostAmount, setBoostAmount] = useState<string>();
+
+  // Router State
   const hasApproval = useHasApproval(
     underlyingTokenAddress,
     TurboAddresses[chainId ?? 1].ROUTER,
     address
   );
   const [hasApprovals, setHasApproval] = useState<boolean>(hasApproval);
-  const [approving, setApproving] = useState(false);
-  const [creatingSafe, setCreatingSafe] = useState(false);
-  const [navigating, setNavigating] = useState(false);
 
-  // Utils
-  const toast = useToast();
-
+  // Modal Logic
   const onClickCreateSafe = async () => {
     if (!address || !provider || !chainId) return;
 
     setCreatingSafe(true);
 
-    // TODO(nathanhleung): enable actual safe creation
     const amountBN = parseEther(depositAmount ?? "0");
 
     let receipt;
@@ -78,7 +78,7 @@ export const CreateSafeModal: React.FC<CreateSafeModalProps> = ({
         handleGenericError(err, toast);
         console.log({ err });
       } finally {
-        // setCreatingSafe(false);
+        setCreatingSafe(false);
       }
     } else {
       try {
@@ -112,6 +112,9 @@ export const CreateSafeModal: React.FC<CreateSafeModalProps> = ({
     setApproving(false);
   }
 
+  const balance = formatEther(collateralBalance)
+
+  // Modal Context
   const createSafeCtx: CreateSafeCtx = {
     provider,
     chainId: chainId ?? 1,
@@ -121,14 +124,13 @@ export const CreateSafeModal: React.FC<CreateSafeModalProps> = ({
     setUnderlyingTokenAddress,
     depositAmount,
     setDepositAmount,
-    boostAmount,
-    setBoostAmount,
     hasApproval,
     approving,
     approve: onClickApprove,
     createSafe: onClickCreateSafe,
     creatingSafe,
     navigating,
+    collateralBalance: balance,
     onClose() {
       // Only allow close if a transaction isn't in progress.
       if (!approving && !creatingSafe) {
@@ -145,6 +147,8 @@ export const CreateSafeModal: React.FC<CreateSafeModalProps> = ({
       }
     },
   };
+
+  
 
   return (
     <Modal
