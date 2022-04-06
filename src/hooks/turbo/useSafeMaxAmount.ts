324546f955f66d2951ceafd6bbf2577b93e124c2
nathanhleung <github@nathanhleung.com>
2022-04-06 12:43:00 -0700

    Fix Boost modal

--- src/hooks/turbo/useSafeMaxAmount.ts
+++ src/hooks/turbo/useSafeMaxAmount.ts
@@ -1,35 +1,36 @@
-import { useRari } from "context/RariContext"
-import { constants } from "ethers"
-import { SafeInfo } from "lib/turbo/fetchers/safes/getSafeInfo"
-import { fetchMaxSafeAmount } from "lib/turbo/utils/fetchMaxSafeAmount"
-import { useQuery } from "react-query"
-import { SafeInteractionMode } from "./useUpdatedSafeInfo"
+import { useRari } from "context/RariContext";
+import { BigNumber, constants } from "ethers";
+import { SafeInfo } from "lib/turbo/fetchers/safes/getSafeInfo";
+import { fetchMaxSafeAmount } from "lib/turbo/utils/fetchMaxSafeAmount";
+import { useQuery } from "react-query";
+import { SafeInteractionMode } from "./useUpdatedSafeInfo";
 
 const useSafeMaxAmount = (
-    safe: SafeInfo | undefined,
-    mode: SafeInteractionMode,
-    strategyIndex?: number //only used for LESS
+  safe: SafeInfo | undefined,
+  mode: SafeInteractionMode,
+  strategyIndex?: number //only used for LESS
 ) => {
-    const { provider, address, chainId } = useRari();
+  const { provider, address, chainId } = useRari();
 
-    const { data: maxAmount } = useQuery(
-        `Safe ${safe?.safeAddress} Max ${mode} amount ${!!strategyIndex ? 'Strategy ' + strategyIndex : null}`,
-        async () => {
-            if (!chainId) return
+  const { data: maxAmount } = useQuery<BigNumber>(
+    `Safe ${safe?.safeAddress} Max ${mode} amount ${
+      !!strategyIndex ? "Strategy " + strategyIndex : null
+    }`,
+    async () => {
+      if (!chainId) return constants.Zero;
 
-            fetchMaxSafeAmount(
-                provider, 
-                mode, 
-                address, 
-                safe, 
-                chainId,
-                strategyIndex
-            )
-        } 
-            
-    )
+      return fetchMaxSafeAmount(
+        provider,
+        mode,
+        address,
+        safe,
+        chainId,
+        strategyIndex
+      );
+    }
+  );
 
-    return maxAmount ?? constants.Zero
-}
+  return maxAmount ?? constants.Zero;
+};
 
-export default useSafeMaxAmount
+export default useSafeMaxAmount;
