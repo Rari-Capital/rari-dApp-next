371354391b1946c48c6a117ca11eb0a03b0325d1
nathanhleung <github@nathanhleung.com>
2022-04-13 19:11:06 -0700

    Add claim interest success state

--- src/components/pages/Turbo/TurboSafePage/modals/ClaimInterestModal/ClaimInterestModal.tsx
+++ src/components/pages/Turbo/TurboSafePage/modals/ClaimInterestModal/ClaimInterestModal.tsx
@@ -1,18 +1,21 @@
 // Hooks
 import { useRari } from "context/RariContext";
+import { useERC4626StrategiesDataAsMap } from "hooks/turbo/useStrategyInfo";
+import { useUserFeiOwed } from "hooks/turbo/useUserFeiOwed";
+import {
+  USDPricedStrategy,
+  USDPricedTurboSafe,
+} from "lib/turbo/fetchers/safes/getUSDPricedSafeInfo";
+import { filterUsedStrategies } from "lib/turbo/fetchers/strategies/formatStrategyInfo";
+import { safeClaimAll } from "lib/turbo/transactions/claim";
+import { safeSweep } from "lib/turbo/transactions/safe";
+import { FEI } from "lib/turbo/utils/constants";
 // Turbo
 import { Modal } from "rari-components";
 import { useState } from "react";
 import { handleGenericError } from "utils/errorHandling";
 import { useToast } from "@chakra-ui/react";
 import { MODAL_STEPS } from "./modalSteps";
-import { useUserFeiOwed } from "hooks/turbo/useUserFeiOwed";
-import { safeClaimAll } from "lib/turbo/transactions/claim";
-import { safeSweep } from "lib/turbo/transactions/safe";
-import { FEI } from "lib/turbo/utils/constants";
-import { USDPricedTurboSafe, USDPricedStrategy } from "lib/turbo/fetchers/safes/getUSDPricedSafeInfo";
-import { filterUsedStrategies } from "lib/turbo/fetchers/strategies/formatStrategyInfo";
-import { useERC4626StrategiesDataAsMap } from "hooks/turbo/useStrategyInfo";
 
 // Todo - reuse Modal Prop Types
 type ClaimInterestModalProps = {
@@ -38,14 +41,22 @@ export const ClaimInterestModal: React.FC<ClaimInterestModalProps> = ({
       setStepIndex(stepIndex + 1);
     }
   }
+  function resetStepIndex() {
+    setStepIndex(0);
+  }
 
-  const activeStrategies: USDPricedStrategy[] = filterUsedStrategies(safe?.usdPricedStrategies ?? []) as USDPricedStrategy[]
-  const strategyData = useERC4626StrategiesDataAsMap(activeStrategies.map(strat => strat.strategy))
+  const activeStrategies: USDPricedStrategy[] = filterUsedStrategies(
+    safe?.usdPricedStrategies ?? []
+  ) as USDPricedStrategy[];
+  const strategyData = useERC4626StrategiesDataAsMap(
+    activeStrategies.map((strat) => strat.strategy)
+  );
 
-  const [totalClaimable, claimableFromStrategies, safeFeiBalance] = useUserFeiOwed(safe)
+  const [totalClaimable, claimableFromStrategies, safeFeiBalance] =
+    useUserFeiOwed(safe);
 
   const onClickClaimInterest = async () => {
-    if (!safe || !chainId || !activeStrategies) return
+    if (!safe || !chainId || !activeStrategies) return;
     try {
       setClaiming(true);
 
@@ -58,17 +69,19 @@ export const ClaimInterestModal: React.FC<ClaimInterestModalProps> = ({
           safeFeiBalance,
           chainId,
           await provider.getSigner()
-        )
-        await tx.wait(1)
+        );
+        await tx.wait(1);
+        incrementStepIndex();
       } else {
         const tx = await safeClaimAll({
           safeAddress: safe.safeAddress,
-          strategies: activeStrategies.map(s => s.strategy),
+          strategies: activeStrategies.map((s) => s.strategy),
           recipient: address,
           signer: await provider.getSigner(),
           chainID: chainId,
-        })
-        await tx.wait(1)
+        });
+        await tx.wait(1);
+        incrementStepIndex();
       }
     } catch (err) {
       handleGenericError(err, toast);
@@ -81,6 +94,7 @@ export const ClaimInterestModal: React.FC<ClaimInterestModalProps> = ({
     <Modal
       ctx={{
         incrementStepIndex,
+        resetStepIndex,
         claiming,
         onClickClaimInterest,
         onClose,
@@ -89,7 +103,7 @@ export const ClaimInterestModal: React.FC<ClaimInterestModalProps> = ({
         safeFeiBalance,
         activeStrategies,
         strategyData,
-        safe
+        safe,
       }}
       isOpen={isOpen}
       onClose={onClose}
