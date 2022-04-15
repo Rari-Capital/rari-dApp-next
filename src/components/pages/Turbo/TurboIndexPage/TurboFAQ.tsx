c188ae5068b57f13a8660b438fb285f2f4ec18be
nathanhleung <github@nathanhleung.com>
2022-04-14 18:05:56 -0700

    Create new index page

--- src/components/pages/Turbo/TurboIndexPage/TurboFAQ.tsx
+++ src/components/pages/Turbo/TurboIndexPage/TurboFAQ.tsx
@@ -1,85 +1,219 @@
-import React from "react";
-import { Heading, Text } from "rari-components";
-import { Box, HStack, Image, Stack } from "@chakra-ui/react";
 import usePreviewSafes from "hooks/turbo/usePreviewSafes";
-import SafeGrid from "./SafeGrid";
+import { Button, Heading, Text } from "rari-components";
+import React from "react";
+import {
+  Box,
+  BoxProps,
+  Center,
+  Flex,
+  Image,
+  SimpleGrid,
+  Stack,
+} from "@chakra-ui/react";
 
-const TurboFAQ = () => {
+type TurboFAQProps = BoxProps;
+
+const TurboFAQ: React.FC<TurboFAQProps> = (props) => {
   const safes = usePreviewSafes();
 
   return (
-    <Stack spacing={12}>
-      <HStack>
-        <Image
-          boxSize={"md"}
-          src="https://media.discordapp.net/attachments/958411922330509314/959902592236929154/otrorr.png?width=1220&height=1220"
-        />
+    <Box {...props}>
+      <Stack spacing={24}>
         <Box>
-          <Heading size="md">How does it work?</Heading>
-          <Text variant="secondary" mt={4}>
-            Turbo can be used by individuals, treasuries, DAOs, protocols, or
-            any on-chain entity. Turbo Safes allow these parties to create a
-            collateralized Fuse position with an approved DeFi token as the
-            primary collateral type.
-            <br />
-            <br />
-            Once the DeFi token is collateralized in a Fuse pool, the owner of
-            this safe can then mint FEI at 0% APR, making this process
-            completely free to the borrower. The FEI is minted at no cost so
-            long as that FEI is supplied into a yield generating strategy that
-            is compliant with ERC-4626, such as: Fuse plug-ins, tokenized
-            vaults, etc.
-            <br />
-            <br />
-            The users of Turbo will most likely deposit this FEI back into their
-            own Fuse pool so that their community can borrow FEI against their
-            collateral types and provide a revenue split to the issuer and the
-            minter (Tribe DAO).
-          </Text>
+          <Heading size="lg">How does Turbo work?</Heading>
+          {/* `Stack` adds a little bit of space between each child, even if
+              `spacing` is set to 0, which we don't want. `Flex` does not. */}
+          <Flex flexDirection={["column", "column", "row"]} mt={12}>
+            <Stack flex={1} spacing={4}>
+              <Heading
+                size="md"
+                variant="success"
+                display="flex"
+                alignItems="center"
+              >
+                <Center
+                  background="success"
+                  boxSize={8}
+                  mr={4}
+                  color="black"
+                  borderRadius="50%"
+                >
+                  1
+                </Center>
+                Create Safe
+              </Heading>
+              <Box background="success" height={1} width="100%" />
+              <Text
+                variant="secondary"
+                // Add a little extra space between the right edge of
+                // this text and the next text.
+                pr={6}
+              >
+                Collateralizing, boosting, depositing, withdrawing, slurping,
+                and sweeping are all isolated features per safe.
+              </Text>
+            </Stack>
+            <Stack flex={1} spacing={4} mt={[12, 12, 0]}>
+              <Heading
+                size="md"
+                variant="success"
+                display="flex"
+                alignItems="center"
+              >
+                <Center
+                  background="success"
+                  boxSize={8}
+                  mr={4}
+                  color="black"
+                  borderRadius="50%"
+                >
+                  2
+                </Center>
+                Deposit Collateral
+              </Heading>
+              <Box background="success" height={1} width="100%" />
+              <Text
+                variant="secondary"
+                // Add a little extra space between the right edge of
+                // this text and the next text.
+                pr={6}
+              >
+                Once the DeFi token is collateralized in a Fuse pool, the owner
+                of this safe can then mint FEI at 0% APR (boosting).
+              </Text>
+            </Stack>
+            <Stack flex={1} spacing={4} mt={[12, 12, 0]}>
+              <Heading
+                size="md"
+                variant="success"
+                display="flex"
+                alignItems="center"
+              >
+                <Center
+                  background="success"
+                  boxSize={8}
+                  mr={4}
+                  color="black"
+                  borderRadius="50%"
+                >
+                  3
+                </Center>
+                Boost FEI
+              </Heading>
+              <Flex position="relative">
+                <Box background="success" height={1} width="110%" />
+                {/* Arrow tip */}
+                {/* https://css-tricks.com/snippets/css/css-triangle/ */}
+                <Box
+                  position="absolute"
+                  top={-2.5}
+                  right={-2.5}
+                  width={0}
+                  height={0}
+                  borderStyle="solid"
+                  borderTopWidth={12}
+                  borderTopColor="transparent"
+                  borderBottomWidth={12}
+                  borderBottomColor="transparent"
+                  borderLeftWidth={12}
+                  borderLeftColor="success"
+                />
+              </Flex>
+              <Text
+                variant="secondary"
+                // Add a little extra space between the right edge of
+                // this text and the next text.
+                pr={6}
+              >
+                FEI is minted at no cost so long as that FEI is supplied into a
+                yield generating strategy that is compliant with ERC-4626, such
+                as: Fuse plug-ins, tokenized vaults, etc.
+              </Text>
+            </Stack>
+          </Flex>
         </Box>
-      </HStack>
-
-      <Box>
-        <Heading size="md">What is a Safe?</Heading>
-        <Text variant="secondary" mt={4}>
-          A safe is a Lorem ipsum dolor sit amet, consectetur adipiscing elit.
-          Aenean vel tellus in velit malesuada pretium. Nulla viverra sodales
-          mauris ut tincidunt. Interdum et malesuada fames ac ante ipsum primis
-          in faucibus. Quisque non eleifend ipsum. Pellentesque non lacinia
-          erat, eu commodo nisl. Morbi felis est, tincidunt eget lorem a,
-          egestas volutpat neque.
-        </Text>
-      </Box>
-
-      <Box>
-        <Heading size="md">Should I use Turbo?</Heading>
-        <Text variant="secondary" mt={4}>
-          DAOs are often seen with large treasuries consisting of their native
-          governance token and very little stablecoins due to the cost and
-          undertaking that must occur. Turbo allows a DAO to become a premier
-          minter of FEI, just as easily as the Tribe DAO. The DAO affiliate will
-          mint a specific percentage of FEI from a collateralized position,
-          while creating a revenue split agreement with the Tribe DAO to keep
-          both organizations incentivized from the yield being earned.
-          <br />
-          <br />
-          If you are a DAO that needs stablecoin liquidity and an alternate
-          source of revenue, Turbo is for you.
-        </Text>
-      </Box>
-
-      <Box>
-        <Heading size="md">How can she slap?</Heading>
-        <Text variant="secondary" mt={4}>
-          Lorem ipsum dolor sit amet, consectetur adipiscing elit. Aenean vel
-          tellus in velit malesuada pretium. Nulla viverra sodales mauris ut
-          tincidunt. Interdum et malesuada fames ac ante ipsum primis in
-          faucibus. Quisque non eleifend ipsum. Pellentesque non lacinia erat,
-          eu commodo nisl. Morbi felis est, tincidunt eget lorem a, egestas
-          volutpat neque.
-        </Text>
-      </Box>
-    </Stack>
+        <Stack
+          direction={["column", "column", "column", "row"]}
+          spacing={[12, 12, 12, 24]}
+        >
+          <Box>
+            <Heading size="lg">Turbo is for everyone</Heading>
+            <Text variant="secondary" mt={4}>
+              Turbo can be used by individuals, treasuries, DAOs, protocols, or
+              any on-chain entity.
+            </Text>
+            <SimpleGrid columns={[1, 1, 2]} mt={8}>
+              <Flex alignItems="center" my={4}>
+                <Image
+                  width={16}
+                  src="/static/turbo/user-icons/individuals.png"
+                />
+                <Text fontSize="lg" ml={6}>
+                  Individuals
+                </Text>
+              </Flex>
+              <Flex alignItems="center" my={4}>
+                <Image width={16} src="/static/turbo/user-icons/daos.png" />
+                <Text fontSize="lg" ml={6}>
+                  DAOs
+                </Text>
+              </Flex>
+              <Flex alignItems="center" my={4}>
+                <Image
+                  width={16}
+                  src="/static/turbo/user-icons/protocols.png"
+                />
+                <Text fontSize="lg" ml={6}>
+                  Protocols
+                </Text>
+              </Flex>
+              <Flex alignItems="center" my={4}>
+                <Image width={16} src="/static/turbo/user-icons/more.png" />
+                <Text fontSize="lg" ml={6}>
+                  And more...
+                </Text>
+              </Flex>
+            </SimpleGrid>
+          </Box>
+          <Image
+            width="md"
+            src="/static/turbo/turbo-engine-3d.png"
+            alignSelf="center"
+          />
+        </Stack>
+        <Stack
+          direction={["column", "column", "column", "row"]}
+          spacing={[12, 12, 12, 24]}
+        >
+          <Box>
+            <Heading size="lg">Utilize your treasury assets</Heading>
+            <Text variant="secondary" mt={4}>
+              Deposit BAL, TRIBE, and gOHM today. Support for more protocol
+              tokens coming soon.
+            </Text>
+          </Box>
+          <Image
+            width="md"
+            src="/static/turbo/treasury-assets.png"
+            alignSelf="center"
+          />
+        </Stack>
+        <Stack spacing={12} alignItems="center" py={12}>
+          <Center flexDirection="column">
+            <Heading size="xl">A completely new financial mechanism </Heading>
+            <Text variant="secondary" fontSize="xl" mt={4}>
+              Turbo changes the relationship between users in an ecosystem and
+              the stablecoin issuer. It’s a powerful transformation in the
+              thinking of stablecoins and their provision throughout the
+              ecosystem.
+            </Text>
+          </Center>
+          <Button size="lg" variant="success">
+            Create a safe
+          </Button>
+        </Stack>
+      </Stack>
+    </Box>
   );
 };
 
--- src/components/pages/Turbo/TurboIndexPage/TurboIndexPage.tsx
+++ src/components/pages/Turbo/TurboIndexPage/TurboIndexPage.tsx
@@ -1,26 +1,26 @@
 import { useIsUserAuthorizedToCreateSafes } from "hooks/turbo/useIsUserAuthorizedToCreateSafes";
+import usePreviewSafes from "hooks/turbo/usePreviewSafes";
 import { useAllUserSafes } from "hooks/turbo/useUserSafes";
+import { TRIBE } from "lib/turbo/utils/constants";
 import { Button, Divider, Heading, Text } from "rari-components";
+import { useRariTokenData } from "rari-components/hooks";
 import { WarningIcon } from "@chakra-ui/icons";
 import {
   Box,
   HStack,
   Image,
   Stack,
-  useDisclosure,
   VStack,
+  useDisclosure,
 } from "@chakra-ui/react";
 import TurboLayout from "../TurboLayout";
 import CreateSafeModal from "./CreateSafeModal/";
-import { TRIBE } from "lib/turbo/utils/constants";
-import usePreviewSafes from "hooks/turbo/usePreviewSafes";
-import { useRariTokenData } from "rari-components/hooks";
 import TurboFAQ from "./TurboFAQ";
 import UserSafes from "./UserSafes";
 
 const TurboIndexPage: React.FC = () => {
   const { isOpen, onOpen, onClose } = useDisclosure();
-  
+
   const safes = useAllUserSafes() ?? [];
   const hasSafes = safes.length > 0;
 
@@ -31,67 +31,74 @@ const TurboIndexPage: React.FC = () => {
   const _ = useRariTokenData(TRIBE);
 
   return (
-    <TurboLayout>
-      <CreateSafeModal onClose={onClose} isOpen={isOpen} />
-      <Stack
-        direction={["column", "column", "row"]}
-        alignItems="center"
-        spacing={12}
-      >
-        <Box flex={1}>
-          <Heading size="xl">Introducing Turbo</Heading>
-          <Text variant="secondary" pt={4} fontSize="xl">
-            Turbo allows any DeFi token to become productive by sharing in the
-            yield generated from approved strategies earning off of a costless
-            FEI line of credit.
-          </Text>
-          {/* Buttons */}
-          <VStack w="100%" align="flex-start">
-            <HStack pt={8} spacing={4} mb={4}>
-              {!hasSafes && (
+    // Hide overflow of main Turbo screenshot image
+    <Box overflowX="hidden" width="100%">
+      <TurboLayout>
+        <CreateSafeModal onClose={onClose} isOpen={isOpen} />
+        <Stack
+          direction={["column", "column", "row"]}
+          alignItems="center"
+          spacing={12}
+        >
+          <Box minWidth={["100%", "100%", "50%"]} pr={[0, 0, 32]}>
+            <Heading size="xl">Introducing Turbo</Heading>
+            <Text pt={4} fontSize="xl">
+              Boost FEI liquidity and earn interest against any protocol token.
+            </Text>
+            {/* Buttons */}
+            <VStack w="100%" align="flex-start">
+              <HStack pt={8} spacing={4} mb={4}>
+                {!hasSafes && (
+                  <Button
+                    variant={isAuthorized ? "success" : "warning"}
+                    onClick={onOpen}
+                    disabled={!isAuthorized}
+                  >
+                    Create a safe
+                  </Button>
+                )}
                 <Button
-                  variant={isAuthorized ? "success" : "warning"}
-                  onClick={onOpen}
-                  disabled={!isAuthorized}
+                  variant="cardmatte"
+                  as="a"
+                  // TypeScript doesn't realize that `as="a"` means that this can
+                  // have an `href` prop.
+                  // @ts-ignore
+                  href="https://medium.com/fei-protocol/the-tribe-dao-strongly-believes-that-a-healthy-and-thriving-defi-ecosystem-needs-a-robust-platform-b1faea700dfa"
+                  target="_blank"
                 >
-                  Create a safe
+                  Learn more
                 </Button>
-              )}
-              <Button
-                variant="cardmatte"
-                as="a"
-                // TypeScript doesn't realize that `as="a"` means that this can
-                // have an `href` prop.
-                // @ts-ignore
-                href="https://medium.com/fei-protocol/the-tribe-dao-strongly-believes-that-a-healthy-and-thriving-defi-ecosystem-needs-a-robust-platform-b1faea700dfa"
-                target="_blank"
-              >
-                Learn more
-              </Button>
-            </HStack>
-            {!hasSafes && !isAuthorized && (
-              <HStack>
-                <WarningIcon color="warning" />
-                <Text color={"warning"}>
-                  Turbo is in beta, and safe creation is only open to authorized
-                  users.
-                </Text>
               </HStack>
-            )}
-          </VStack>
+              {!hasSafes && !isAuthorized && (
+                <HStack>
+                  <WarningIcon color="warning" />
+                  <Text color={"warning"}>
+                    Turbo is in beta, and safe creation is only open to
+                    authorized users.
+                  </Text>
+                </HStack>
+              )}
+            </VStack>
+          </Box>
+          <Box flex={1}>
+            <Box position="relative" top={0} left={0}>
+              <Image
+                src="/static/turbo/turbo.png"
+                width={["100%", "100%", "55vw"]}
+                maxWidth={["100%", "100%", "55vw"]}
+              />
+            </Box>
+          </Box>
+        </Stack>
+        <Box pt={24}>
+          {hasSafes ? (
+            <UserSafes safes={safes} onClickCreateSafe={onOpen} />
+          ) : (
+            <TurboFAQ />
+          )}
         </Box>
-        <Box flex={1}>
-          <Image src="/static/turbo/turbo.png" />
-        </Box>
-      </Stack>
-
-      <Divider mt={20} mb={16} />
-      {hasSafes ? (
-        <UserSafes safes={safes} onClickCreateSafe={onOpen} />
-      ) : (
-        <TurboFAQ />
-      )}
-    </TurboLayout>
+      </TurboLayout>
+    </Box>
   );
 };
 
