16eaca6c535954fc3d76f8736bb75675bf1b0e21
Sharad <sharadshekar@gmail.com>
2022-04-20 11:57:19 -0500

    tryin it out

--- src/pages/turbo/safe/[id].tsx
+++ src/pages/turbo/safe/[id].tsx
@@ -3,11 +3,29 @@ import TurboSafePage from "components/pages/Turbo/TurboSafePage/TurboSafePage";
 import { serverSideTranslations } from "next-i18next/serverSideTranslations";
 import { useRouter } from "next/router";
 import Head from "next/head";
+import { chooseBestWeb3Provider } from "utils/web3Providers";
+import { createTurboSafe } from "lib/turbo/utils/turboContracts";
+import { getSafeInfo } from "lib/turbo/fetchers/safes/getSafeInfo";
+import { fetchTokenData } from "hooks/useTokenData";
 
-export async function getStaticProps({ locale }: { locale: string }) {
+export async function getStaticProps({
+  locale,
+  params,
+}: {
+  locale: string;
+  params: any;
+}) {
+  const { id } = params;
+  const provider = chooseBestWeb3Provider();
+  const turboSafe = createTurboSafe(provider, id);
+  const collateral = await turboSafe.callStatic.asset();
+  const { symbol } = await fetchTokenData(collateral);
+
   return {
     props: {
       ...(await serverSideTranslations(locale)),
+      safeId: id,
+      symbol,
     },
   };
 }
@@ -19,25 +37,31 @@ export const getStaticPaths = async () => {
   };
 };
 
-const Page: NextPage = () => {
-  const {
-    query: { id },
-  } = useRouter();
+const Page: NextPage<{
+  safeId: string;
+  symbol: string;
+}> = ({ safeId, symbol }) => {
   return (
     <>
       <Head>
         <title> Safe | Tribe Turbo</title>
         <meta property="twitter:card" content="summary" />
-        <meta property="og:title" content={`Safe| Tribe Turbo`} />
-        <meta name="twitter:title" content={`Safe | Tribe Turbo`} />
+        <meta property="og:title" content={`${symbol} Safe | Tribe Turbo`} />
+        <meta name="twitter:title" content={`${symbol} Safe | Tribe Turbo`} />
         <meta
           property="og:url"
-          content={`https://app.rari.capital/turbo/safe/${id}`}
+          content={`https://app.rari.capital/turbo/safe/${safeId}`}
         />
         <meta property="og:image" content="/static/turbo/turbo2.png" />
         <meta property="twitter:image" content="/static/turbo/turbo2.png" />
-        <meta property="og:description" content="Earn costless yield and boost FEI liquidity with Tribe Turbo." />
-        <meta property="twitter:description" content="Earn costless yield and boost liquidity with Tribe Turbo." />
+        <meta
+          property="og:description"
+          content="Earn costless yield and boost FEI liquidity with Tribe Turbo."
+        />
+        <meta
+          property="twitter:description"
+          content="Earn costless yield and boost liquidity with Tribe Turbo."
+        />
       </Head>
       <TurboSafePage />
     </>
